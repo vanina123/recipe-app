74ac6beb8089cfef988f1d46d10631c576b6a6fe
Nana Vanina <vaninalarissa@gmail.com>
2023-08-09 09:14:07 +0100

    insert more data in the file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# User.create!(name: 'John Doe', email: 'john@example.com', password: 'password')
+# User.create!(name: 'Jane Smith', email: 'jane@example.com', password: 'password')
+
+# Create Recipes
+Recipe.create!(name: 'Vanilla Cake', preparation_time: Time.now, cooking_time: 20,
+               description: 'Delicious vaninlla cake recipe.', public: true, user_id: 1)
+Recipe.create!(name: 'Vegetable Stir-Fry', preparation_time: Time.now, cooking_time: 30,
+               description: 'Healthy vegetable stir-fry recipe.', public: true, user_id: 2)
