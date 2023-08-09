4909a0142854d883dc78459c13bdd56f1be6ad98
eudondian <eudondian@gmail.com>
2023-08-09 19:05:33 +0100

    Modify user_controller file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  def shopping_list
+  end   
 
-  def show
-    @user = current_user
-  end
-
-  def edit
-    @user = current_user
-  end
-
-  def update
-    @user = current_user
-
-    if @user.update(user_params)
-      redirect_to user_path, notice: 'Profile successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email)
-  end
 end
