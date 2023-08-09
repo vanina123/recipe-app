5f455be80fda23963a943a3b04e69921290049d0
eudondian <eudondian@gmail.com>
2023-08-09 11:33:50 +0100

    Modify routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users
+  root to: "users#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-end
+  resources :users
+  resources :foods, only: %i[index new create destroy]
+  resources :recipees, only: %i[index show new create destroy] do
+    member do
+      patch :toggle_public
+    end
+    resources :recipe_foods, only: %i[new create destroy] 
+  end
