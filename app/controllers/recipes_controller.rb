d9c92b68d54392f06d0f80888b184b05575127bf
Nana Vanina <vaninalarissa@gmail.com>
2023-08-09 16:44:15 +0100

    Fix linter error

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,48 +1,47 @@
 class RecipesController < ApplicationController
-    def index
-      @recipes = Recipe.all
-    end
-  
-    def show
-      @recipe = Recipe.find(params[:id])
-    end
-  
-    def new
-      @recipe = Recipe.new
-      
-    end
-  
-    def create
-      @recipe = Recipe.new(recipe_params)
-      if @recipe.save
-        redirect_to @recipe
-      else
-        render :new
-      end
-    end
-  
-    def edit
-      @recipe = Recipe.find(params[:id])
-    end
-  
-    def update
-      @recipe = Recipe.find(params[:id])
-      if @recipe.update(recipe_params)
-        redirect_to @recipe
-      else
-        render :edit
-      end
-    end
-  
-    def destroy
-      @recipe = Recipe.find(params[:id])
-      @recipe.destroy
-      redirect_to recipes_path
-    end
-  
-    private
-  
-    def recipe_params
-      params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+  def index
+    @recipes = Recipe.all
+  end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to @recipe
+    else
+      render :new
     end
   end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    if @recipe.update(recipe_params)
+      redirect_to @recipe
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:name, :preparation_time, :cooking_time, :description, :public, :user_id)
+  end
+end
