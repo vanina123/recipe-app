c614ca36a924b4c325e873a99049ea5e6f1e055f
Nana Vanina <vaninalarissa@gmail.com>
2023-08-10 05:14:55 +0100

    Implementing N+1 querries

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,9 @@
 class UsersController < ApplicationController
   def shopping_list
     @user = current_user
-    @recipes = @user.recipes.includes(recipe_foods: [:food])
+    @recipes = @user.recipes.includes(recipe_foods: :food)
 
-    @missing_foods = []
-    @recipes.each do |recipe|
-      @missing_foods += recipe.missing_foods
-    end
+    @missing_foods = RecipeFood.includes(:food).where(recipe: @recipes).to_a
 
     @total_food_items = @missing_foods.sum(&:quantity)
     @total_price = @missing_foods.sum { |recipe_food| recipe_food.quantity * recipe_food.food.price }
